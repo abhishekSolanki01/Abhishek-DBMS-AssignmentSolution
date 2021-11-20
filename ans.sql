c4b59ecbfb47a87c7271b575b56d7086351431e6
abhishek <abhishek.solanki1110@gmail.com>
2021-11-20 12:12:15 +0530

    initial commit

--- ans.sql
+++ ans.sql
@@ -50,25 +50,50 @@ insert into `PRICE` values ("Sitting" 1500 1860)
 
 select count(Gender) 
 from PASSENGER
-where Gender='F' and Distance>='600'
+where Gender='F' and Distance>='600';
 
 select count(Gender) 
 from PASSENGER
-where Gender='M' and Distance>='600'
+where Gender='M' and Distance>='600';
 
 -- 4
 select min(Price)
-from PRICE
+from PRICE;
 
 --5) Select passenger names whose names start with character 'S'
 select Passenger_name
 from PASSENGER
-where Passenger_name like "S%"
+where Passenger_name like "S%";
 
 --6) Calculate price charged for each passenger displaying Passenger name, Boarding City,
 -- Destination City, Bus_Type, Price in the output
 
-select 
+select r.Price, p.Passenger_name, p.Boarding_City, p.Destination_City, p.Bus_Type
 from PASSENGER p left join PRICE r on p.Bus_Type = r.Bus_Type
-where p.Distance = r.Distance
+where p.Distance = r.Distance;
+
+-- 7.) What is the passenger name and his/her ticket price who travelled in Sitting bus for a
+-- distance of 1000 KM s
+
+select p.Passenger_name, r.Price
+from PASSENGER p inner join PRICE r on p.Bus_Type = r.Bus_Type
+where p.Distance = 1000 and p.Bus_Type = "Sitting";
+
+-- 8) What will be the Sitting and Sleeper bus charge for Pallavi to travel from Bangalore to
+-- Panaji?
+
+select r.Price, p.Bus_Typ
+from PASSENGER p inner join PRICE r on p.Bus_Type = r.Bus_Typ
+where p.Passenger_name = "Pallavi" and p.Boarding_City="Bangalore" and p.Destination_City="Panaji";
+
+--9) List the distances from the "Passenger" table which are unique (non-repeated
+-- distances) in descending order.
+
+select distinct Distance
+from PASSENGER 
+order by Distance des 
+
+-- 10) Display the passenger name and percentage of distance travelled by that passenger
+-- from the total distance travelled by all passengers without using user variables 
+
 
